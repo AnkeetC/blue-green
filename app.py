f3bcaf70b4743b6caa5ea4b76ab2533e755f4633
AnkeetC <115153160+AnkeetC@users.noreply.github.com>
2023-10-14 12:12:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return 'Hello, World! I am blue
 
 @app.route('/about')
 def about():
-    return 'This is the about page.'
+    return 'This is blue Environment.'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
